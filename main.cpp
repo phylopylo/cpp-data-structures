02df2dd41ad2c401edc87e17c942103cbc7e449d
Philip Roberts <philip@philip.science>
2026-09-23 14:15:01 -0400

    switch to use shared pointers universally

--- main.cpp
+++ main.cpp
@@ -18,25 +18,31 @@ std::shared_ptr<Node> exampleTree() {
 	// Use Nodes to construct an example tree
 	std::shared_ptr<Node> root = std::make_shared<Node>(3);
 	root->left = std::make_shared<Node>(8);
+	root->right = std::make_shared<Node>(4);
+	root->left->left = std::make_shared<Node>(6);
+	root->left->right = std::make_shared<Node>(4);
+	root->right->left = std::make_shared<Node>(7);
+	root->right->right = std::make_shared<Node>(2);
 	return root;
 };
 
-void printTreeBFS(std::shared_ptr<Node> node) {
-	std::vector<std::shared_ptr<Node>> queue{node};
+void printTreeBFS(std::shared_ptr<Node>& node) {
+	std::vector<std::shared_ptr<Node>> queue;
+	queue.push_back(node);
+
 	std::string output;
 	unsigned int i(0);
 	while(i < queue.size()) {
 
-		Node* cur = queue[i];
-		output += std::format("{}\n", cur->value);
+		std::shared_ptr<Node> cur = queue[i];
+		output += std::format("{} ", cur->value);
 
-
-		std::unique_ptr<Node> L = cur->left; // Left Child 
+		std::shared_ptr<Node> L = cur->left; // Left Child 
 		if(L != nullptr) {
 			queue.push_back(L);
 		}
 
-		std::unique_ptr<Node> R = cur->right; // Right Child
+		std::shared_ptr<Node> R = cur->right; // Right Child
 		if(R != nullptr) {
 			queue.push_back(R);
 		}
@@ -46,7 +52,7 @@ void printTreeBFS(std::shared_ptr<Node> node) {
 }
 
 int main() {
-	std::unique_ptr<Node> tree = exampleTree();
+	std::shared_ptr<Node> tree = exampleTree();
 	std::cout << "Printing BFS Search of Example Tree!" << std::endl << std::endl;
 	printTreeBFS(tree);
 	return 0;
